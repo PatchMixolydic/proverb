c21e7a2db8d46a380d2b41ff1cdafe7475fd4536
Ruby Lazuli <nipkraps@gmail.com>
2021-08-15 18:37:05 -0500

    install: elevate if necessary
    Can't just run the script with `sudo` since the user might not have
    Cargo installed for root

--- install.sh
+++ install.sh
@@ -11,9 +11,38 @@ set -eu
 # to be possible with `cargo install`.
 export PREFIX=${1:-${PREFIX:-${CARGO_INSTALL_ROOT:-${CARGO_HOME:-"${HOME}/.cargo"}}}}
 
+try_elevate()  {
+    if [ -n `which sudo` ]; then
+        read -p "Cannot install to '$PREFIX'. Try elevating? [y/N] " response
+        case $response in
+            [Yy]* )
+                # TODO: deduplicate
+                sudo install -m 755 -D -t ${PREFIX}/bin/ target/release/proverb
+                sudo install -m 644 -D -t ${PREFIX}/share/proverb/ proverb/*
+                exit 0;;
+
+            * ) exit 1;;
+        esac
+    else
+        echo "Cannot install to '$PREFIX'. 'sudo' doesn't appear to be installed, so this script can't elevate for you."
+        echo "If you have an alternative sudo-like (such as 'doas') installed, try setting an alias temporarily."
+        echo "Otherwise, run the following commands as a user with write permissions for '$PREFIX':"
+        echo
+        echo "install -m 755 -D -t ${PREFIX}/bin/ target/release/proverb"
+        echo "install -m 644 -D -t ${PREFIX}/share/proverb/ proverb/*"
+        exit 1
+    fi
+}
+
+try_run() {
+    if ! $@ ; then
+        try_elevate $@
+    fi
+}
+
 # `cargo clean` so we can bake the correct path into the binary
-cargo clean
+cargo clean --release
 cargo build --release
 strip target/release/proverb
-install -m 755 -D -t ${PREFIX}/bin/ target/release/proverb
-install -m 644 -D -t ${PREFIX}/share/proverb/ proverb/*
+try_run install -m 755 -D -t ${PREFIX}/bin/ target/release/proverb
+try_run install -m 644 -D -t ${PREFIX}/share/proverb/ proverb/*
